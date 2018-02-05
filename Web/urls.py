463fd2cb2e12feeac3cc8dbaa2a99226384ba989
petercdev <P.Cooke@reading.ac.uk>
2018-02-05 16:21:00 +0000

    Route url mapped

--- Web/urls.py
+++ Web/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 
 from django.urls import include, path
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('schedule/', include('schedule.urls')),
+    path('', RedirectView.as_view(url='/schedule/')),    
     path('admin/', admin.site.urls)
 ]
