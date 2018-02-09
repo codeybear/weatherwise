9e2b6d3c095875bfc3b42814ffc5c01ad36b00b1
webdev <33625330+webdev@users.noreply.github.com>
2018-02-09 16:55:00 +0000

    Location JavaScript complete

--- schedule/models/Common.py
+++ schedule/models/Common.py
@@ -3,7 +3,7 @@ import pymysql
 def getconnection():
     return pymysql.connect(host='localhost',
                              user='root',
-                             password='',
+                             password='root',
                              db='weather',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
