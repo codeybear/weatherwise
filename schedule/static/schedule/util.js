c4f216e1cb496ad1d2137a2e50e8ec10a267502a
Anonymous <P.Cooke@reading.ac.uk>
2018-02-21 11:36:09 +0000

    Parsley bootstrap config removed

--- schedule/static/schedule/util.js
+++ schedule/static/schedule/util.js
@@ -36,16 +36,3 @@ function isValidDate(dateString)
   // Check the range of the day
   return day > 0 && day <= monthLength[month - 1];
 };  
-
-$("form").parsley({
-  errorClass: 'has-danger',
-  successClass: 'has-success',
-  classHandler: function(ParsleyField) {
-      return ParsleyField.$element.parents('.form-group');
-  },
-  errorsContainer: function(ParsleyField) {
-      return ParsleyField.$element.parents('.form-group');
-  },
-  errorsWrapper: '<span class="text-help">',
-  errorTemplate: '<div></div>'
-});
