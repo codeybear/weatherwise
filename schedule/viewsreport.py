93a5fa9c0477e0f3fc2b9fb5227f0cc939326bad
webdev <33625330+coder@users.noreply.github.com>
2018-03-06 13:15:12 +0000

    Timing information for reports

--- schedule/viewsreport.py
+++ schedule/viewsreport.py
@@ -4,9 +4,16 @@ from django.template import loader
 
 from schedule.models import Weather
 
+import time
+
 def index(request, schedule_id):
     weather = Weather(schedule_id)
+
+    start_time = time.time()
+    
     activities = weather.CalcScheduleDuration()
+    
+    print("My program took" + str(time.time() - start_time) + "to run")
     
     template = loader.get_template('report/index.html')
     context = { 'activities' : activities }
