a6cbb887304b5ac728ffc975bb475265c28bdf6b
Anonymous <P.Cooke@reading.ac.uk>
2018-03-21 12:37:18 +0000

    Dependency code written

--- schedule/viewsreport.py
+++ schedule/viewsreport.py
@@ -11,20 +11,20 @@ def index(request, schedule_id):
     weather = Weather(schedule_id)
     activities = weather.CalcScheduleDuration()
     
-    # activityService = ActivityService
-    # activities = activityService.GetByScheduleId(schedule_id)
-
-    # start_time = time.time()
-    # today = datetime.datetime.now()
-
-    # for x in range(1,1000000):
-    #     today += datetime.timedelta(days=1)
-
-    # for x in range(1,100):
-    #     activities = weather.CalcScheduleDuration()
-    # print(today)
-    # print("Program took " + str(time.time() - start_time) + " to run")
-    
     template = loader.get_template('report/index.html')
-    context = { 'activities' : activities }
+    context = { 'activities' : activities, 'dependencies' : weather.dependencyList }
     return HttpResponse(template.render(context, request))
+
+# activityService = ActivityService
+# activities = activityService.GetByScheduleId(schedule_id)
+
+# start_time = time.time()
+# today = datetime.datetime.now()
+
+# for x in range(1,1000000):
+#     today += datetime.timedelta(days=1)
+
+# for x in range(1,100):
+#     activities = weather.CalcScheduleDuration()
+# print(today)
+# print("Program took " + str(time.time() - start_time) + " to run")
