3cc5841b099760b567d9c421ce786b0f9029e8de
webdev <33625330+coder@users.noreply.github.com>
2018-03-23 11:10:31 +0000

    vagrant file added

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# base box setup:
+#sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password saintsfc'
+#sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password saintsfc'
+#apt-get update
+#apt-get install -y mysql-server
