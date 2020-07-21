530a97f58847ed10dba6937433178bf0861183a0
Peter C <30731499+codeybear@users.noreply.github.com>
2020-07-21 15:14:22 +0100

    Update viewsreport.py

--- schedule/viewsreport.py
+++ schedule/viewsreport.py
@@ -100,7 +100,7 @@ def CalcReverseReport(scheduleId):
 
     return result
 
-
+# TODO move this into model
 def GetStatusDate(scheduleId):
     scheduleService = ScheduleService
     schedule = scheduleService.GetById(scheduleId)
