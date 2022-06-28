fbe878f39e24626c0f973cad9d88a41d3e682b52
Peter C <30731499+codeybear@users.noreply.github.com>
2022-06-28 11:27:48 +0100

    Delete bootstrap.sh

--- bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# base box setup:
-#sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password'
-#sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password'
-#apt-get update
-#apt-get install -y mysql-server
